33038f1aa52947e53c9e5d9f9d02548084e0a80a
gopi <pgopirmd@gmail.com>
2016-11-13 21:22:57 +0530

    whether leap year or not

---
+++ LeapYear.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Guvi_Beginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a year: ");
+            string userInput = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(userInput) && !string.IsNullOrEmpty(userInput))
+            {
+                if (userInput.Length != 4)
+                {
+                    Console.WriteLine("year should be in 4 character length.");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    int _isLeapYear = Convert.ToInt32(userInput);
+
+                    if (_isLeapYear % 4 == 0 || _isLeapYear % 400 == 0)
+                    {
+                        Console.WriteLine("the given year is a leap year");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("the given year is not a leap year");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
