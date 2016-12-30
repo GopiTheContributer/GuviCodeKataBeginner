8cfd70b7188d0ec4faa40770aa7e4a79db87c42a
gopi <pgopirmd@gmail.com>
2016-12-30 19:34:14 +0530

    odd number between intervals

---
+++ beginnerset2fourthprobem.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int firstInput = int.Parse(Console.ReadLine().Trim());
+        int secondInput = int.Parse(Console.ReadLine().Trim());
+
+        if (firstInput != null && secondInput != null && firstInput != 0 && secondInput != 0
+            && firstInput > 0 && secondInput > 0)
+        {
+            string result = null;
+            if (firstInput > secondInput)
+            {
+                Console.WriteLine("First input should be greater than the second input.");
+                Console.ReadKey();
+                return;
+            }
+
+            for (int i = 1; i <= secondInput; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    if (result == null)
+                        result = i.ToString();
+                    else
+                        result += ", " + i.ToString();
+                }
+            }
+            Console.WriteLine("{0}", result);
+            Console.ReadKey();
+            return;
+        }
+        Console.WriteLine("not a valid input");
+        Console.ReadKey();
+    }
+}
