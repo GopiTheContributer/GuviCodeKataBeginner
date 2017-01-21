fd92942bcc8c9b7cc9dc23a9678ec4903f76b136
Gopi <pgopirmd@gmail.com>
2017-01-21 19:54:36 +0530

    reverse a given string

---
+++ reversestring.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		string userInput = Console.ReadLine().Trim();
+		string output = string.Empty;
+		if(!string.IsNullOrEmpty(userInput)){
+			char[] temp = userInput.ToCharArray();
+			Array.Reverse(temp);
+			foreach(char item in temp){
+				output += item;
+			}
+			Console.WriteLine("{0}",output);
+			Console.ReadLine();
+		}
+		else
+		{
+			Console.WriteLine("Not a valid input");
+			Console.ReadKey();
+		}
+	}
+}
