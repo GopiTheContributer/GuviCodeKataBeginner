38079fc991cd1bb50d0a9327ca3db3299ec04059
Gopi P <pgopirmd@gmail.com>
2019-03-21 07:21:52 +0530

    Update learn.cs

--- learn.cs
+++ learn.cs
@@ -1,1 +1,25 @@
 
+using System;
+
+namespace Guvi_Beginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if (userInput != 0)
+            {
+                if (userInput > 0)
+                    Console.WriteLine("the given number is positive.");
+                else
+                    Console.WriteLine("the given number is negative.");
+            }
+            else
+                Console.WriteLine("the given number is zero");
+            Console.ReadKey();
+        }
+    }
+}
