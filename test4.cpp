2b05c548248f81b2d2fbdfa6447a48454c5cdd2c
Gopi P <pgopirmd@gmail.com>
2019-04-08 07:42:01 +0530

    Update test4.cpp

--- test4.cpp
+++ test4.cpp
@@ -1,1 +1,1 @@
-#include<bits/stdc++.h> \n using namespace std; int main() { int a, b , c; cin >> a >> b >> c; cout << a + b + c << endl; return 0;} 
+#include<bits/stdc++.h> \n using namespace std; int main() { int a, b, c, d; cin >> a >> b >> c >> d; cout << a + b + c + d << endl; return 0;} 
