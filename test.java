4cfcdb86f4bba0db78a7e2490e9d823d205bbc78
Gopi P <pgopirmd@gmail.com>
2019-04-15 11:17:02 +0530

    Update test.java

--- test.java
+++ test.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 class CheckNumber {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextLine();
-		if(input > "Saturday"){
+		string input = sc.nextLine();
+		if(input == "Saturday"){
 		    System.out.println("Yes");
 		}
 	}
