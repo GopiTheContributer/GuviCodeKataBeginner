51053c8014b7a2ea9284a9149c8b96347a985e47
Gopi P <pgopirmd@gmail.com>
2019-04-29 11:44:03 +0530

    Update and rename publicerror.java to class.java

---
+++ class.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+class CheckNumber {
+public static void main (String[] args) 
+{Scanner sc = new Scanner(System.in);
+int inDatra = sc.nextInt();
+if(inDatra > 0)
+{System.out.println("Positive");
+}
+else if(inDatra == 0){ System.out.println("Zero");}
+else{ System.out.println("Negative");}}}
