428ba638d9f2384089c206cf2e2263fdad95f35e
Gopi P <pgopirmd@gmail.com>
2019-04-29 11:47:31 +0530

    Update class.java

--- class.java
+++ class.java
@@ -2,10 +2,9 @@ import java.io.*;
 import java.util.Scanner;
 class CheckNumber {
 public static void main (String[] args) 
-{Scanner sc = new Scanner(System.in);
-int inDatra = sc.nextInt();
-if(inDatra > 0)
-{System.out.println("Positive");
+{
+  for(int i = 0; i<5; i++)
+  {
+    System.out.println("Hello");
+  }
 }
-else if(inDatra == 0){ System.out.println("Zero");}
-else{ System.out.println("Negative");}}}
