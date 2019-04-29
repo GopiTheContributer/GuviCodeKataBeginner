0cd1b7277730525fcbfc1f6c50474d6d75d2622a
Gopi P <pgopirmd@gmail.com>
2019-04-29 11:54:43 +0530

    Update and rename class.java to classhello.java

--- classhello.java
+++ classhello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class CheckNumber {
 public static void main (String[] args) 
 {
-  for(int i = 0; i<5; i++)
+  for(int i = 0; i<15; i++)
   {
     System.out.println("Hello");
   }
