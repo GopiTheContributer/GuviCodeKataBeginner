1e41bb795c75ac2725173b9ff69a33b4e21b1b95
Gopi P <pgopirmd@gmail.com>
2019-04-30 11:49:44 +0530

    Update and rename Even7.cs to singleline.cs

---
+++ singleline.cs
@@ -0,0 +1,1 @@
+using System;namespace Guvibeginprogram{public class Programasdf{static void Main(){int userInput = Convert.ToInt32(Console.ReadLine().Trim());if (userInput > 0){if ((userInput % 2) == 0){Console.WriteLine("the given number is even");}else{Console.WriteLine("The given number is odd.");}}else Console.WriteLine("number should be in positive");Console.ReadKey();}}}
